483c33eb6cc67cfed21ee16f7c7829f62603b82c
Alexander Wang <alexander.wang22@gmail.com>
2020-03-21 12:03:40 -0400

    can submit a theme

--- src/components/Test.js
+++ src/components/Test.js
@@ -4,18 +4,48 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
+import Input from "@material-ui/core/Input";
 // import { GridListTile } from '@material-ui/core';
-import firebase from "firebase-app";
-import db from "./Home";
+// import firebase from "firebase-app";
+// import db from "./Home";
+
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+import firebaseConfig from "../config/firebase";
+
+//TESTING FIREBASE DATABASE CONNECTION HERE, WE WILL MOVE THIS
+firebase.initializeApp(firebaseConfig);
+export const db = firebase.firestore();
+db.settings({ timestampsInSnapshots: true });
 
 const useStyles = makeStyles(theme => ({}));
 
 export function Test(props) {
-  const palette = {}; // import this from build
+  // const palette = {}; // import this from build
   const [primary, setPrimary] = useState("");
   const [secondary, setSecondary] = useState("");
-  palette.primary = primary;
-  palette.secondary = secondary;
+  // palette.primary = primary;
+  // palette.secondary = secondary;
+  const palette = { primary, secondary };
+
+  console.log("Test -> palette", palette);
+
+  /// TESTING CONNECTION TO CUSTOMIZEDTHEMES
+  // db.collection("CustomizedThemes")
+  //   .get()
+  //   .then(snapshot => {
+  //     snapshot.docs.forEach(doc => {
+  //       console.log("Test -> doc", doc.data());
+  //       // theme.palette = doc.data().palette;
+  //       // theme.typography = doc.data().typography;
+  //     });
+  //   })
+  //   .catch(err => {
+  //     console.log("Error getting documents", err);
+  //   });
+  ///
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -58,23 +88,25 @@ export function Test(props) {
           justify="center"
           alignItems="center"
         >
-          <Grid item container justify="center">
-            <TextField
+          {/* <Grid item container justify="center"> */}
+          <form onSubmit={handleSubmit}>
+            <Input
               label="Palette Primary"
               id="primary"
               value={primary}
               onChange={e => setPrimary(e.target.value)}
             />
-            <TextField
+            <Input
               label="Palette Secondary"
               id="secondary"
               value={secondary}
               onChange={e => setSecondary(e.target.value)}
             />
-          </Grid>
-          <Button>
-            <input type="submit" value="Post object" onSubmit={handleSubmit} />
-          </Button>
+            {/* </Grid> */}
+            <Button>
+              <input type="submit" value="Post object" />
+            </Button>
+          </form>
         </Grid>
       </Grid>
     </Grid>
