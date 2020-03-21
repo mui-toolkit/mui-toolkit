bb1e16811b8b2cbefcd28628e43c44149adc3ab7
Rana Quadri <rq59@ymail.com>
2020-03-21 17:19:43 -0400

    Login/Logout/Signup authentication working

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,25 @@
-import React from "react";
-import Header from "../components/ui/Header";
-import { ThemeProvider } from "@material-ui/styles";
-import theme from "./ui/Theme";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from "./Home";
-import Learn from "./Learn";
-import Login from "./Login";
-import Signup from "./Signup";
-import Build from "./build/Build";
-import Test from "./Test";
+import React from 'react';
+import Header from '../components/ui/Header';
+import { ThemeProvider } from '@material-ui/styles';
+import theme from './ui/Theme';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import Learn from './Learn';
+import Login from './Login';
+import Signup from './Signup';
+import Build from './build/Build';
+import Test from './Test';
+import firebase from 'firebase';
+import 'firebase/auth';
+
+firebase.auth().onAuthStateChanged(user => {
+  console.log('user', user);
+  if (user) {
+    console.log('user logged in:', user);
+  } else {
+    console.log('user logged out');
+  }
+});
 
 function App() {
   return (
