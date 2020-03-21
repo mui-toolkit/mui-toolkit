8a09cd65467e00d03d5d900e41245c5d26eacb65
Alexander Wang <alexander.wang22@gmail.com>
2020-03-20 22:12:45 -0400

    database connected to homepage to test schema

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,48 @@
-import React from 'react';
+import React from "react";
+
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+import firebaseConfig from "../config/firebase";
+import { unaryExpression } from "@babel/types";
+
+firebase.initializeApp(firebaseConfig);
+export const db = firebase.firestore();
+db.settings({ timestampsInSnapshots: true });
 
 export default function Home() {
+  const user = {};
+  const theme = {};
+  db.collection("Users")
+    .get()
+    .then(snapshot => {
+      snapshot.docs.forEach(doc => {
+        // console.log("Home -> doc", doc.data().firstName);
+        user.firstName = doc.data().firstName;
+        user.lastName = doc.data().lastName;
+        user.email = doc.data().email;
+        user.themes = doc.data().themes;
+      });
+    });
+
+  db.collection("CustomizedThemes")
+    .get()
+    .then(snapshot => {
+      snapshot.docs.forEach(doc => {
+        // console.log("Home -> doc", doc.data());
+        theme.palette = doc.data().palette;
+        theme.typography = doc.data().typography;
+      });
+    });
+  console.log("Home -> user", user, theme);
+  // return !user ? (
+  //   <div>Home</div>
+  // ) : (
+  //   <div>
+  //     Home
+  //     {user.firstName}
+  //   </div>
+  // );
   return <div>Home</div>;
 }
---
+++ src/config/firebase.js
@@ -0,0 +1,23 @@
+// import firebase from "firebase/app";
+// import firebase from "firebase/firestore";
+// import "firebase/auth";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBevfCK5ecPcHaddoc3ISZJaqiICw4OXx8",
+  authDomain: "mui-theme.firebaseapp.com",
+  databaseURL: "https://mui-theme.firebaseio.com",
+  projectId: "mui-theme",
+  storageBucket: "mui-theme.appspot.com",
+  messagingSenderId: "910262165775",
+  appId: "1:910262165775:web:51ef7d1564b7a52ae05e7a",
+  measurementId: "G-C66ZLEV61P"
+};
+// firebase.initializeApp(firebaseConfig);
+// firebase.firestore();
+// const db = firebase.firestore();
+// db.settings({ timestampsInSnapshots: true });
+
+// export default { firebase, db };
+
+export default firebaseConfig;
