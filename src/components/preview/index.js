38b0fb2cdf0d3661a076d1384a26b645163eed3a
Nelson Liu <nelson.ls.liu@gmail.com>
2020-03-23 17:45:25 -0400

    refactored state into store component, moved saved theme to its own component

---
+++ src/components/preview/index.js
@@ -0,0 +1,8 @@
+/**
+ * `components/index.js` exists simply as a 'central export' for our components.
+ * This way, we can import all of our components from the same place, rather than
+ * having to figure out which file they belong to!
+ */
+export { PreviewAppBar } from './PreviewAppBar';
+export { PreviewButton } from './PreviewButton';
+export { PreviewTypography } from './PreviewTypography';
