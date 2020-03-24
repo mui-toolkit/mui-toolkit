14127bacc733caf7aa0b02e492a94a4ae1157de5
Alexander Wang <alexander.wang22@gmail.com>
2020-03-24 07:57:45 -0400

    added a user profile. needs to appear in the dashboard though

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,47 @@
-import React from 'react';
-import Header from '../components/ui/Header';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './Home';
-import Learn from './Learn';
-import Login from './Login';
-import Signup from './Signup';
-import UsersThemes from './UsersThemes';
-import SavedThemes from './SavedThemes';
-import UserDashboard from './UserDashboard';
-import auth from './auth';
-import firebase from 'firebase';
-import 'firebase/auth';
-import { Store } from './build/';
+import React from "react";
+import Header from "../components/ui/Header";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import Learn from "./Learn";
+import Login from "./Login";
+import Signup from "./Signup";
+import UsersThemes from "./UsersThemes";
+import SavedThemes from "./SavedThemes";
+import UserDashboard from "./UserDashboard";
+import UserProfile from "./UserProfile";
+import auth from "./auth";
+import firebase from "firebase";
+import "firebase/auth";
+import { Store } from "./build/";
 
-firebase.auth().onAuthStateChanged((user) => {
-	console.log('user', user);
-	if (user) {
-		console.log('user logged in:', user);
-	} else {
-		console.log('user logged out');
-	}
+firebase.auth().onAuthStateChanged(user => {
+  console.log("user", user);
+  if (user) {
+    console.log("user logged in:", user);
+  } else {
+    console.log("user logged out");
+  }
 });
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Header />
-			<Switch>
-				<Route exact path="/" component={Home} />
-				<Route exact path="/learn" component={Learn} />
-				<Route exact path="/design" component={Store} />
-				<Route exact path="/login" component={Login} />
-				<Route exact path="/logout" component={() => <div>logout</div>} />
-				<Route exact path="/signup" component={Signup} />
-				{/* <Route exact path="/test" component={Test} /> */}
-				<Route exact path="/auth" component={auth} />
-				<Route exact path="/usersthemes" component={UsersThemes} />
-				<Route exact path="/savedthemes" component={SavedThemes} />
-				<Route exact path="/userdashboard" component={UserDashboard} />
-			</Switch>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/learn" component={Learn} />
+        <Route exact path="/design" component={Store} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/logout" component={() => <div>logout</div>} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/auth" component={auth} />
+        <Route exact path="/usersthemes" component={UsersThemes} />
+        <Route exact path="/savedthemes" component={SavedThemes} />
+        <Route exact path="/userdashboard" component={UserDashboard} />
+        <Route exact path="/userprofile" component={UserProfile} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -6,7 +6,6 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import Drawer from "@material-ui/core/Drawer";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import List from "@material-ui/core/List";
 import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
@@ -178,7 +177,7 @@ export default function UserDashboard() {
             </ListItemIcon>
             <ListItemText primary="Dashboard" />
           </ListItem>
-          <ListItem button>
+          <ListItem button component={Link} to="/userprofile">
             <ListItemIcon>
               <FaceIcon />
             </ListItemIcon>
