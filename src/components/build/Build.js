84b112bcfbcf630e229763626c9930ab245d2e06
Manny Garcia <mannyalgarcia@gmail.com>
2020-03-23 23:23:50 -0400

    feat(build/build.js): added an expansion panel for UX navigation through build process
    Made a component in BuildNav.js to hold this expansion panel

--- src/components/build/Build.js
+++ src/components/build/Build.js
@@ -1,111 +1,97 @@
 import React from 'react';
-import { Palette, SaveTheme } from '../build';
+import { Palette, SaveTheme, BuildNav } from '../build';
 import { PreviewButton, PreviewTypography, PreviewAppBar } from '../preview';
 import Download from '../Download';
 
-import { Grid, Paper } from '@material-ui/core/';
-import { makeStyles } from '@material-ui/styles';
+import { Grid, Paper, Typography, Avatar } from '@material-ui/core/';
+import { makeStyles, withStyles } from '@material-ui/styles';
 
-const useStyles = makeStyles((theme) => ({
-	root: {
-		// display: 'flex',
-		// flex: 3,
-		// flexDirection: 'column',
-	},
-	// buttonRoot: {
-	//   padding: theme.spacing.unit,
-	// },
-	// selector: {
-	//   // alignSelf: 'center',
-	//   // margin: theme.spacing.unit,
-	// },
-	container: {
-		// align: 'center',
-		flex: 1,
-		overflow: 'auto',
-		width: '50%'
-		// background: '#000',
-	},
-	builderPaper: {
-		padding: '1em',
-		marginTop: '5em',
-		textAlign: 'center',
-		background: '#fff'
-		// color: theme.palette.text.secondary,
-	},
-	previewPaper: {
-		padding: '5em',
-		marginTop: '2em',
-		textAlign: 'center',
-		background: '#fff'
-
-		// color: theme.palette.text.secondary,
-		// height: '100vh',
-		// background: '#000',
-	}
+const useStyles = makeStyles(theme => ({
+  preview: {
+    padding: '2em',
+  },
+  previewPaper: {
+    marginTop: '5em',
+    textAlign: 'center',
+    background: '#fff',
+  },
+  builderPaper: {
+    marginTop: '5em',
+    textAlign: 'center',
+    background: '#fff',
+  },
+  expansionPanel: {
+    background: '#fff',
+  },
 }));
 
-export const Build = (props) => {
-	const classes = useStyles();
+export const Build = props => {
+  const classes = useStyles();
 
-	const {
-		color,
-		secondaryColor,
-		defaultColor,
-		paperColor,
-		changeColor,
-		changeSecondaryColor,
-		changePaperColor,
-		changeDefaultColor,
-		downloadTheme
-	} = props;
+  const {
+    color,
+    secondaryColor,
+    defaultColor,
+    paperColor,
+    changeColor,
+    changeSecondaryColor,
+    changePaperColor,
+    changeDefaultColor,
+    downloadTheme,
+  } = props;
 
-	// needs a themeName pop up so user can name their theme and it will be referenced in the table of Saved Themes.  .collection('CT').doc(`${themeName}`.set({})) should create a new collection in CustomizedThemes with doc name themeName and allow the collection to contain any/all fields
+  // needs a themeName pop up so user can name their theme and it will be referenced in the table of Saved Themes.  .collection('CT').doc(`${themeName}`.set({})) should create a new collection in CustomizedThemes with doc name themeName and allow the collection to contain any/all fields
 
-	return (
-		<section className={classes.root}>
-			<Grid container>
-				<Grid item className={classes.selector}>
-					<Paper className={classes.builderPaper}>
-						<Palette
-							color={color}
-							secondaryColor={secondaryColor}
-							defaultColor={defaultColor}
-							paperColor={paperColor}
-							changeColor={changeColor}
-							changeSecondaryColor={changeSecondaryColor}
-							changeDefaultColor={changeDefaultColor}
-							changePaperColor={changePaperColor}
-						/>
-						{/* Theme Builder End */}
-					</Paper>
-				</Grid>
-				<Grid item className={classes.container}>
-					<Paper className={classes.previewPaper} style={{ background: `${defaultColor}` }}>
-						{/* Preview Start */}
+  return (
+    <section className={classes.root}>
+      <Grid container spacing={1}>
+        {/* BUILD START */}
+        <Grid item xs={3} className={classes.selector}>
+          <Paper className={classes.builderPaper}>
+            <BuildNav />
+            <Grid item>
+              <Palette
+                color={color}
+                secondaryColor={secondaryColor}
+                defaultColor={defaultColor}
+                paperColor={paperColor}
+                changeColor={changeColor}
+                changeSecondaryColor={changeSecondaryColor}
+                changeDefaultColor={changeDefaultColor}
+                changePaperColor={changePaperColor}
+              />
+            </Grid>
+          </Paper>
+        </Grid>
+        {/* BUILD END */}
+        {/* Preview Start */}
+        <Grid item xs={9} className={classes.container}>
+          <Paper
+            className={classes.previewPaper}
+            style={{ background: `${defaultColor}` }}
+          >
+            <PreviewAppBar
+              secondaryColor={secondaryColor}
+              color={color}
+              className={classes.container}
+            />
+            <Grid item className={classes.preview}>
+              <Grid container>
+                <Grid item xs={12}>
+                  <PreviewButton />
+                </Grid>
+                <Grid item xs={12}>
+                  <PreviewTypography />
+                </Grid>
+              </Grid>
+            </Grid>
 
-						<Grid item>
-							<PreviewAppBar
-								secondaryColor={secondaryColor}
-								color={color}
-								className={classes.container}
-							/>
-							<Grid container spacing={1}>
-								<Grid item xs={12}>
-									<PreviewButton />
-								</Grid>
-								<Grid item xs={12}>
-									<PreviewTypography />
-								</Grid>
-							</Grid>
-						</Grid>
-
-						<Download downloadTheme={downloadTheme} />
-						{/* Preview End */}
-						<SaveTheme downloadTheme={downloadTheme} />
-					</Paper>
-				</Grid>
-			</Grid>
-		</section>
-	);
+            <Download downloadTheme={downloadTheme} />
+            <SaveTheme downloadTheme={downloadTheme} />
+          </Paper>
+        </Grid>
+        {/* Preview End */}
+      </Grid>
+    </section>
+  );
 };
--- src/components/preview/PreviewTypography.js
+++ src/components/preview/PreviewTypography.js
@@ -4,61 +4,65 @@ import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 
 export const PreviewTypography = () => {
-	const useStyles = makeStyles((theme) => ({
-		paper: {
-			background: ''
-		}
-	}));
-	const classes = useStyles();
+  const useStyles = makeStyles(theme => ({
+    paper: {
+      background: '',
+    },
+  }));
+  const classes = useStyles();
 
-	return (
-		<React.Fragment>
-			<Paper className={classes.paper}>
-				<Typography variant="h4">TYPOGRAPHY</Typography>
-				<Typography variant="h1" component="h2" gutterBottom>
-					h1. Heading
-				</Typography>
-				<Typography variant="h2" gutterBottom>
-					h2. Heading
-				</Typography>
-				<Typography variant="h3" gutterBottom>
-					h3. Heading
-				</Typography>
-				<Typography variant="h4" gutterBottom>
-					h4. Heading
-				</Typography>
-				<Typography variant="h5" gutterBottom>
-					h5. Heading
-				</Typography>
-				<Typography variant="h6" gutterBottom>
-					h6. Heading
-				</Typography>
-				<Typography variant="subtitle1" gutterBottom>
-					subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-				</Typography>
-				<Typography variant="subtitle2" gutterBottom>
-					subtitle2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-				</Typography>
-				<Typography variant="body1" gutterBottom>
-					body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde
-					suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos
-					laborum fugiat deleniti? Eum quasi quidem quibusdam.
-				</Typography>
-				<Typography variant="body2" gutterBottom>
-					body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde
-					suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos
-					laborum fugiat deleniti? Eum quasi quidem quibusdam.
-				</Typography>
-				<Typography variant="button" display="block" gutterBottom>
-					button text
-				</Typography>
-				<Typography variant="caption" display="block" gutterBottom>
-					caption text
-				</Typography>
-				<Typography variant="overline" display="block" gutterBottom>
-					overline text
-				</Typography>
-			</Paper>
-		</React.Fragment>
-	);
+  return (
+    <React.Fragment>
+      <Paper className={classes.paper}>
+        <Typography variant='h4'>TYPOGRAPHY</Typography>
+        <Typography variant='h1' component='h2' gutterBottom>
+          h1. Heading
+        </Typography>
+        <Typography variant='h2' gutterBottom>
+          h2. Heading
+        </Typography>
+        <Typography variant='h3' gutterBottom>
+          h3. Heading
+        </Typography>
+        <Typography variant='h4' gutterBottom>
+          h4. Heading
+        </Typography>
+        <Typography variant='h5' gutterBottom>
+          h5. Heading
+        </Typography>
+        <Typography variant='h6' gutterBottom>
+          h6. Heading
+        </Typography>
+        <Typography variant='subtitle1' gutterBottom>
+          subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Quos blanditiis tenetur
+        </Typography>
+        <Typography variant='subtitle2' gutterBottom>
+          subtitle2. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Quos blanditiis tenetur
+        </Typography>
+        <Typography variant='body1' gutterBottom>
+          body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+          blanditiis tenetur unde suscipit, quam beatae rerum inventore
+          consectetur, neque doloribus, cupiditate numquam dignissimos laborum
+          fugiat deleniti? Eum quasi quidem quibusdam.
+        </Typography>
+        <Typography variant='body2' gutterBottom>
+          body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+          blanditiis tenetur unde suscipit, quam beatae rerum inventore
+          consectetur, neque doloribus, cupiditate numquam dignissimos laborum
+          fugiat deleniti? Eum quasi quidem quibusdam.
+        </Typography>
+        <Typography variant='button' display='block' gutterBottom>
+          button text
+        </Typography>
+        <Typography variant='caption' display='block' gutterBottom>
+          caption text
+        </Typography>
+        <Typography variant='overline' display='block' gutterBottom>
+          overline text
+        </Typography>
+      </Paper>
+    </React.Fragment>
+  );
 };
