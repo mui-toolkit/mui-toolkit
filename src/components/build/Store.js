4fde33c1f7ab20171a568a5cba06d39c6c364ada
Nelson Liu <nelson.ls.liu@gmail.com>
2020-03-24 22:13:22 -0400

    added button handlers

--- src/components/build/Store.js
+++ src/components/build/Store.js
@@ -17,15 +17,15 @@ export const Store = () => {
 	//Buttons
 	const [ buttonRipple, setButtonRipple ] = useState(false);
 	const [ buttonElevation, setButtonElevation ] = useState(false);
-	const [ buttonHoverColor, setButtonHoverColor ] = useState('');
-	const [ buttonHoverOpacity, setButtonHoverOpacity ] = useState('');
-	const [ buttonFontWeight, setButtonFontWeight ] = useState('');
-	const [ buttonFontSize, setButtonFontSize ] = useState('');
+	const [ buttonHoverColor, setButtonHoverColor ] = useState('#000000');
+	const [ buttonHoverOpacity, setButtonHoverOpacity ] = useState(0.04);
+	const [ buttonFontWeight, setButtonFontWeight ] = useState(500);
+	const [ buttonFontSize, setButtonFontSize ] = useState('0.875');
 	//none, capitalize, uppercase, lowercase, initial, inherit
-	const [ buttonTextTransform, setButtonTestTransform ] = useState('');
-	const [ buttonHeight, setButtonHeight ] = useState('');
-	const [ buttonPadding, setButtonPadding ] = useState('');
-	const [ buttonBorderRadius, setButtonBorderRadius ] = useState('');
+	const [ buttonTextTransform, setButtonTestTransform ] = useState('none');
+	const [ buttonHeight, setButtonHeight ] = useState(48);
+	const [ buttonPadding, setButtonPadding ] = useState('0 30px');
+	const [ buttonBorderRadius, setButtonBorderRadius ] = useState(0);
 
 	//Material-UI states
 	const [ expanded, setExpanded ] = useState('panel1');
@@ -35,33 +35,58 @@ export const Store = () => {
 	const changeColor = (color) => {
 		setColor(color.hex);
 	};
-
 	const changeSecondaryColor = (secondaryColor) => {
 		setSecondaryColor(secondaryColor.hex);
 	};
-
 	const changeDefaultColor = (defaultColor) => {
 		setDefaultColor(defaultColor.hex);
 	};
-
 	const changePaperColor = (paperColor) => {
 		setPaperColor(paperColor.hex);
 	};
-
 	const changeColorPickerDisplayed = () => {
 		setDisplayColorPicker(!displayColorPicker ? true : false);
 	};
-
 	const changeSecondaryColorPickerDisplayed = () => {
 		setDisplaySecondaryColorPicker(!displaySecondaryColorPicker ? true : false);
 	};
-
 	const changeDefaultColorPickerDisplayed = () => {
 		setDisplayDefaultColorPicker(!displayDefaultColorPicker ? true : false);
 	};
-
 	const changePaperColorPickerDisplayed = () => {
 		setDisplayPaperColorPicker(!displayPaperColorPicker ? true : false);
+	};
+
+	//Buttons handlers
+	const changeButtonRipple = () => {
+		setButtonRipple(!buttonRipple ? true : false);
+	};
+	const changeButtonElevation = () => {
+		setButtonElevation(!buttonElevation ? true : false);
+	};
+	const changeButtonHoverColor = (buttonHoverColor) => {
+		setButtonHoverColor(buttonHoverColor.hex);
+	};
+	const changeButtonHoverOpacity = (buttonHoverOpacity) => {
+		setButtonHoverOpacity(buttonHoverOpacity);
+	};
+	const changeButtonFontWeight = (buttonFontWeight) => {
+		setButtonFontWeight(buttonFontWeight);
+	};
+	const changeButtonFontSize = (buttonFontSize) => {
+		setButtonFontSize(buttonFontWeight);
+	};
+	const changeButtonTextTransform = (buttonTextTransform) => {
+		setButtonTextTransform(buttonTextTransform);
+	};
+	const changeButtonHeight = (buttonHeight) => {
+		setButtonHeight(buttonHeight);
+	};
+	const changeButtonPadding = (buttonPadding) => {
+		setButtonPadding(buttonPadding);
+	};
+	const changeButtonBorderRadius = (buttonBorderRadius) => {
+		setButtonBorderRadius(buttonBorderRadius);
 	};
 
 	//Material-Ui handlers
@@ -75,22 +100,53 @@ export const Store = () => {
 
 	let downloadTheme = {
 		palette: {
-			primary: { main: `${color}` ? `${color}` : '#3f51b5' },
+			primary: { main: `${color}` },
 			secondary: {
-				main: `${secondaryColor}` ? `${secondaryColor}` : '#f50057'
+				main: `${secondaryColor}`
 			},
 			background: {
 				paper: `${paperColor}`,
 				default: `${defaultColor}`
+			},
+			action: {
+				// active: 'rgba(0, 0, 0, 0.54)',
+				hover: `${buttonHoverColor}`,
+				hoverOpacity: `${buttonHoverOpacity}`
+				// selected: 'rgba(0, 0, 0, 0.04)',
+				// selectedOpacity: 0.08
+			}
+		},
+		typography: {
+			button: {
+				fontWeight: `${buttonFontWeight}`,
+				fontSize: `${buttonFontSize}`,
+				textTransform: `${buttonTextTransform}`
+			}
+		},
+		props: {
+			MuiButtonBase: {
+				disableRipple: `${buttonRipple}`
+			},
+			MuiButton: {
+				disableElevation: `${buttonElevation}`
+			}
+		},
+		overrides: {
+			MuiButton: {
+				root: {
+					borderRadius: `${buttonBorderRadius}`,
+					height: `${buttonHeight}`,
+					padding: `${buttonPadding}`
+				}
 			}
 		}
 	};
 
 	const customTheme = createMuiTheme({
 		palette: {
-			primary: { main: `${color}` ? `${color}` : '#3f51b5' },
+			primary: { main: `${color}` },
 			secondary: {
-				main: `${secondaryColor}` ? `${secondaryColor}` : '#f50057'
+				main: `${secondaryColor}`
 			},
 			background: {
 				paper: `${paperColor}`,
