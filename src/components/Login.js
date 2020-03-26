992644cedf303fb6f55021914a63a88ef9333b26
Rana Quadri <rq59@ymail.com>
2020-03-26 14:48:48 -0400

    Added login error toast message

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,87 +1,169 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import firebase from 'firebase';
 import 'firebase/auth';
-import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@material-ui/core/';
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Button,
+  Snackbar,
+  IconButton
+} from '@material-ui/core/';
+import CloseIcon from '@material-ui/icons/Close';
 
-const useStyles = makeStyles((theme) => ({ button: { marginRight: '20px' } }));
+const useStyles = makeStyles(theme => ({ button: { marginRight: '20px' } }));
 
 export function Login(props) {
-	const [ email, setEmail ] = useState('');
-	const [ password, setPassword ] = useState('');
-	const [ open, setOpen ] = useState(false);
-	const classes = useStyles();
-	const handleClickOpen = () => {
-		setOpen(true);
-	};
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [openSnack, setOpenSnack] = useState(false);
+  const [open, setOpen] = useState(false);
 
-	const handleCancel = (e) => {
-		setOpen(false);
-	};
+  const handleClick = () => {
+    setOpenSnack(true);
+  };
+  const handleClose = (event, reason) => {
+    console.log('hanCl', event, reason);
+    if (reason === 'clickaway' || reason === 'timeout') {
+      setOpenSnack(false);
+      return;
+    }
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		firebase
-			.auth()
-			.signInWithEmailAndPassword(email, password)
-			.then((cred) => {
-				console.log(cred.user);
-			})
-			.catch(function(error) {
-				console.log('error in login', error.code);
-			});
-		// props.history.push('/');
-	};
+    setOpen(false);
+  };
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-	return (
-		<div>
-			<Button
-				label="Login"
-				// variant="outlined"
-				// color="primary"
-				onClick={handleClickOpen}
-			>
-				Login
-			</Button>
-			<Dialog open={open} onClose={handleCancel} aria-labelledby="form-dialog-title">
-				{' '}
-				<DialogTitle id="form-dialog-title">LOG IN</DialogTitle>
-				{/* <DialogContentText>
-          Name your saved theme something quirky
-        </DialogContentText> */}
-				{/* <DialogTitle id="form-dialog-title">Email</DialogTitle> */}
-				<DialogContent>
-					<TextField
-						label="Email"
-						id="email"
-						value={email}
-						onChange={(e) => setEmail(e.target.value)}
-						required
-					/>
-				</DialogContent>
-				{/* <DialogTitle id="form-dialog-title">Password</DialogTitle> */}
-				<DialogContent>
-					<TextField
-						type="password"
-						label="Password"
-						id="password"
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-					/>
-				</DialogContent>
-				<DialogActions>
-					<Button style={{ marginRight: '20px' }} onClick={handleSubmit} color="primary">
-						Login
-					</Button>
-					<Button style={{ marginRight: '20px' }} onClick={handleCancel} color="primary">
-						Cancel
-					</Button>
-				</DialogActions>
-			</Dialog>
-		</div>
-	);
+  const handleCancel = e => {
+    setOpen(false);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(cred => {
+        console.log(cred.user);
+      })
+      .catch(function(error) {
+        if (!isEmpty(error)) {
+          handleClick();
+        }
+        console.log('error in login', error);
+        // alert(error.message);
+        alert('Invalid username or password');
+      });
+  };
+
+  const validate = email => {
+    let errors = {};
+    if (!email) {
+      errors = 'Email address is required';
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      errors = 'Email address is invalid';
+    }
+    return errors;
+  };
+  const isEmpty = obj => {
+    for (var key in obj) {
+      if (obj.hasOwnProperty(key)) return false;
+    }
+    return true;
+  };
+
+  return (
+    <div>
+      <Button
+        label="Login"
+        // variant="outlined"
+        // color="primary"
+        onClick={handleClickOpen}
+      >
+        Login
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleCancel}
+        aria-labelledby="form-dialog-title"
+      >
+        {' '}
+        <DialogTitle id="form-dialog-title">LOG IN</DialogTitle>
+        <DialogContent>
+          <TextField
+            label="Email"
+            id="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            required
+            error={!isEmpty(validate(email)) && email.length > 0}
+            helperText={
+              validate(email) && email.length > 0
+                ? 'Please enter a valid email'
+                : ''
+            }
+            helperText={
+              !isEmpty(validate(email)) && email.length > 0
+                ? validate(email)
+                : ''
+            }
+          />
+        </DialogContent>
+        <DialogContent>
+          <TextField
+            type="password"
+            label="Password"
+            id="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button
+            style={{ marginRight: '20px' }}
+            onClick={handleSubmit}
+            color="primary"
+          >
+            Login
+          </Button>
+          <Button
+            style={{ marginRight: '20px' }}
+            onClick={handleCancel}
+            color="primary"
+          >
+            Cancel
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'center',
+          horizontal: 'center'
+        }}
+        open={openSnack}
+        autoHideDuration={4000}
+        onClose={handleClose}
+        message="Invalid Username or Password."
+        action={
+          <React.Fragment>
+            <IconButton
+              size="small"
+              aria-label="close"
+              color="inherit"
+              onClick={handleClose}
+            >
+              <CloseIcon fontSize="small" />
+            </IconButton>
+          </React.Fragment>
+        }
+      />
+    </div>
+  );
 }
 
 export default Login;
