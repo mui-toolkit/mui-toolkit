d3516bbe2f12f2f57cb849704aa03758b79031b9
Manny Garcia <mannyalgarcia@gmail.com>
2020-03-29 17:03:45 -0400

    feat(webpreview.js store.js): set loading screen for web preview and design page
    design and web preview page have a backdrop loading spinner while data is being set

--- src/components/build/Store.js
+++ src/components/build/Store.js
@@ -8,7 +8,6 @@ import { shadowTrue, shadowFalse } from './Shadows';
 import Backdrop from '@material-ui/core/Backdrop';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles(theme => ({
   backdrop: {
