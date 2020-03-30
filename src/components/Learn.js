7e66af88f6664e602ff4151bb7484ac9b9d5f6e3
Rana Quadri <rq59@ymail.com>
2020-03-29 21:53:48 -0400

    Fixed DOM nesting warning in Learn component

--- src/components/Learn.js
+++ src/components/Learn.js
@@ -7,19 +7,19 @@ import { Typography, Paper, Link } from '@material-ui/core';
 const useStyles = makeStyles({
   root: {
     color: 'black',
-    height: '100%',
+    height: '100%'
   },
   title: {
-    padding: '1em',
+    padding: '1em'
   },
   container: {
     paddingTop: '5em',
     paddingLeft: '200px',
-    paddingRight: '200px',
+    paddingRight: '200px'
   },
   tab: {
-    textIndent: '30px',
-  },
+    textIndent: '30px'
+  }
 });
 
 export default function Learn() {
@@ -30,34 +30,34 @@ export default function Learn() {
       <div className={classes.root}>
         <Grid
           container
-          direction='column'
-          alignItems='center'
+          direction="column"
+          alignItems="center"
           className={classes.container}
         >
           <Paper elevation={0}>
-            <Grid item align='center' className={classes.title}>
-              <img alt='mui logo' src={muiLogo} style={{ width: '20%' }} />
+            <Grid item align="center" className={classes.title}>
+              <img alt="mui logo" src={muiLogo} style={{ width: '20%' }} />
             </Grid>
             <Grid item className={classes.title}>
-              <Typography variant='h4' align='center'>
+              <Typography variant="h4" align="center">
                 What is Material UI?
               </Typography>
             </Grid>
             <Grid item>
-              <Typography variant='body1' align='center'>
+              <Typography variant="body1" align="center">
                 Material UI is a popular React UI Framework with 55.5k stars on
                 Github. Visit{' '}
-                <Link href='https://material-ui.com/'>Material UI</Link> to
+                <Link href="https://material-ui.com/">Material UI</Link> to
                 learn more.
               </Typography>
             </Grid>
             <Grid item className={classes.title}>
-              <Typography variant='h4' align='center'>
+              <Typography variant="h4" align="center">
                 What is mymui?
               </Typography>
             </Grid>
             <Grid item>
-              <Typography variant='body1'>
+              <Typography variant="body1" component={'span'}>
                 <div className={classes.tab}>
                   mymui allows you to customize your Material UI theme without
                   having to look at the documentation. The interface is user
@@ -65,26 +65,26 @@ export default function Learn() {
                   you are customizing. Once finished, you can download the json
                   file and copy an paste the code into your own file.
                 </div>
-                <p className={classes.tab}>
+                <div className={classes.tab}>
                   mymui utilizes the{' '}
-                  <Link href='https://material-ui.com/customization/theming/'>
+                  <Link href="https://material-ui.com/customization/theming/">
                     Theme Provider
                   </Link>{' '}
                   component that allows you to inject a theme into your
                   application. To learn more about Material UI{' '}
-                  <Link href='https://material-ui.com/customization/theming/'>
+                  <Link href="https://material-ui.com/customization/theming/">
                     Theming
                   </Link>{' '}
                   and the{' '}
-                  <Link href='https://material-ui.com/customization/theming/'>
+                  <Link href="https://material-ui.com/customization/theming/">
                     Theme Provider
                   </Link>{' '}
                   component, visit the documentation{' '}
-                  <Link href='https://material-ui.com/customization/theming/'>
+                  <Link href="https://material-ui.com/customization/theming/">
                     here
                   </Link>
                   .
-                </p>
+                </div>
               </Typography>
             </Grid>
           </Paper>
