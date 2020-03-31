8401e09e1fe8188d7485fc0469eb81e5e4b9dab9
Rana Quadri <rq59@ymail.com>
2020-03-30 23:25:36 -0400

    Set up Sign up with Github

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -28,6 +28,8 @@ export function Signup(props) {
   const [errors, setErrors] = useState({});
   const [open, setOpen] = React.useState(false);
   var provider = new firebase.auth.GoogleAuthProvider();
+  var providerGH = new firebase.auth.GithubAuthProvider();
+  providerGH.addScope('repo');
   const handleClick = () => {
     setOpen(true);
   };
@@ -234,6 +236,57 @@ export function Signup(props) {
                 alt="logo"
               />
               Sign up with Google
+            </button>
+
+            <button
+              onClick={() => {
+                firebase
+                  .auth()
+                  .signInWithPopup(providerGH)
+                  .then(function(result) {
+                    // This gives you a GitHub Access Token.
+                    var token = result.credential.accessToken;
+                    // The signed-in user info.
+                    var user = result.user;
+                    console.log('GH user', user);
+                    db.collection('Users')
+                      .doc(user.uid)
+                      .set({
+                        email: user.email,
+                        themes: []
+                      });
+                  })
+                  .catch(function(error) {
+                    // Handle Errors here.
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                    // The email of the user's account used.
+                    var email = error.email;
+                    // The firebase.auth.AuthCredential type that was used.
+                    var credential = error.credential;
+                    if (
+                      errorCode ===
+                      'auth/account-exists-with-different-credential'
+                    ) {
+                      alert(
+                        'You have signed up with a different provider for that email.'
+                      );
+                      // Handle linking here if your app allows it.
+                    } else {
+                      console.error(error);
+                    }
+                  });
+              }}
+              className="googleBtn"
+              type="button"
+              style={{ fontSize: 14, marginRight: '20px' }}
+            >
+              <img
+                style={{ width: '30px', marginRight: '5px' }}
+                src="https://upload.wikimedia.org/wikipedia/commons/5/54/GitHub_Logo.png"
+                alt="logo"
+              />
+              Sign up with Github
             </button>
           </Grid>
         </form>
